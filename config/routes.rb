6059cc615552794c3398151b5f7e572b450beca4
David Georgiou <david.georgiou@gmail.com>
2018-10-21 23:16:25 +0100

    controller and view scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/check_mobile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
